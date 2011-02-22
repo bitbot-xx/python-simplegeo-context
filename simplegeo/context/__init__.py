91eeb415095f99f5f462b039190686929b38b914
Doug Dixon <doug@preciado.home>
2011-02-22 18:14:48 +0000

    An implementation of Client that executes requests asynchronously using Google App Engine URL Fetch service, instead of synchronously using httplib2.
    Only one of the context methods is currently implemented.

--- simplegeo/context/__init__.py
+++ simplegeo/context/__init__.py
@@ -6,7 +6,13 @@ import urllib
 
 from simplegeo.shared import json_decode
 from simplegeo.shared import Client as SGClient, is_valid_ip, is_valid_lat, is_valid_lon
+from simplegeo.shared import APIError
 from pyutil.assertutil import precondition
+import oauth2 as oauth
+try:
+    from google.appengine.api import urlfetch
+except:
+    pass
 
 class Client(SGClient):
     def __init__(self, key, secret, api_version=API_VERSION, host="api.simplegeo.com", port=80):
@@ -50,3 +56,48 @@ class Client(SGClient):
         endpoint = self._endpoint('context_by_address', address=urllib.quote_plus(address))
         return json_decode(self._request(endpoint, "GET")[1])
 
+
+class AppEngineClient(Client):
+    """
+    An implementation of Client that executes requests
+    asynchronously using Google App Engine's URL Fetch service, instead of
+    synchronously using httplib2.
+    
+    Example usage:
+    
+    import simplegeo.context as context
+    client = context.AppEngineClient(mykey, mysecret)
+    rpc = client.get_context_async(lat, lon)
+    
+    # some other stuff do to...
+    # ...
+    # now I need the context, this call blocks for it if it hasn't already returned:
+    
+    context = client.get_context_result(rpc)
+    
+    """
+    
+    def get_context_async(self, lat, lon, **kwds):
+        """
+        Starts an asynchronous request using GAE's URL Fetch service.
+        Returns an RPC object that can be subsequently passed to get_context_result()
+        
+        **kwds
+            Keyword arguments to pass when creating the RPC object.
+            See http://code.google.com/appengine/docs/python/urlfetch/asynchronousrequests.html#create_rpc
+        """
+        
+        rpc = urlfetch.create_rpc(**kwds)
+        precondition(is_valid_lat(lat), lat)
+        precondition(is_valid_lon(lon), lon)
+        endpoint = self._endpoint('context', lat=lat, lon=lon)
+        headers = self._headers(endpoint, 'GET')
+        urlfetch.make_fetch_call(rpc, endpoint, headers=headers)
+        return rpc
+        
+        
+    def get_context_result(self, rpc):
+        result = rpc.get_result()
+        if not 200 <= result.status_code < 400:
+            raise APIError(result.status_code, result.content, self.headers)
+        return json_decode(result.content)
